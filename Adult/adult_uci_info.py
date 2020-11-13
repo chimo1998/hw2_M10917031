6f365ca7dc6edd793e6b031d784b628bcf32a185
Tsai Wei-Hsuan <whtsai1998@gmail.com>
2020-11-13 20:24:49 +0800

    Add UCI loader for adult

---
+++ Adult/adult_uci_info.py
@@ -0,0 +1,14 @@
+import uci_loader
+
+class Adult():
+    def __init__(self):  
+        self.url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
+        self.names = ['age','workclass','fnlwgt','education','education-num','marital-status','occupation','relationship','race','sex','capital-gain','capital-loss','hours-per-week','native-country','salary']
+        self.drop = 'fnlwgt'
+        self.encode_tag = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country']
+        self.label_tag = 'salary'
+    def load(self):
+        uci = uci_loader.Loader(self.url, self.names, self.label_tag, self.drop, self.encode_tag)
+        return uci()
+    def __call__(self):
+        return self.load()
